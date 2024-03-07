80a6ead3c98148bd7b79bc61e7e333f362de92a0
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2024-03-07 08:25:53 -0500

    listo

--- App.tsx
+++ App.tsx
@@ -1,118 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type RootStackParamList = {
+ Home: undefined;
+ Home2: undefined;
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Home = ({ navigation }) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+     <SafeAreaView style={styles.SafeAreaView}>
+       <View>
+       <Image
+ source={require('./src/view/loginImage.jpg')}
+/>
+         <Text>Hola, Bienvenido a Linkcafe!</Text>
+       </View>
+       <TouchableOpacity onPress={() => navigation.navigate('Home2')} style={styles.button}>
+         <Text>Vamos a la siguiente vista</Text>
+       </TouchableOpacity>
+     </SafeAreaView>
   );
-}
+ };
+const Home2 = ({navigation}) => {
+ return (
+    <SafeAreaView style={styles.SafeAreaView}>
+      <View>
+        <Text>Hola, esta es la sgunda  viista</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
+        <Text>Vamos a la anterior vista</Text>
+      </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+ );
+};
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+ return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home2" component={Home2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+ SafeAreaView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+ },
+ button: {
+    backgroundColor: 'orange',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+ },
 });
 
 export default App;
---
+++ src/view/segunda.tsx
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const vista = () => {
+ return (
+    <View>
+      <Text>Hola, mundo!</Text>
+    </View>
+ );
+};
+
+export default vista;
